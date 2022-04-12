ee48439d8184f51290aae7022b79fb2daf3b1ae8
Abhaysingh077 <abhay.singh@walkingtree.tech>
2022-04-12 15:34:42 +0530

    msg

--- src/main/java/com/student/HelloProgramApplication.java
+++ src/main/java/com/student/HelloProgramApplication.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class HelloProgramApplication {
  
-	@GetMapping("/m")
-	public String m() {
-		return "hello welcome in azure";
-	}
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloProgramApplication.class, args);
